0efe9234117fe372da9e05d34388954f56015eb6
MohamedAboMousallam <86533055+MohamedAboMousallam@users.noreply.github.com>
2022-01-04 23:32:39 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var currentTime = new Date();
+    var year = currentTime.getFullYear();
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("Rates").value;
+    var t = document.getElementById("Yearss").value;
+    var A = p * (1+ r * t);
+   document.getElementById("__message").innerHTML=`if you deposit ${p}, \n at interest rate ${r}. \n You will recieve an amount of ${A},\n in the year ${year + parseInt(t)}`; 
 }
+
         
